c97e90f24848525897ada971690b58fdf12eafa4
กัมปนาท เก่งพรสกุล < >
2016-09-17 00:20:43 +0700

    add program path

--- generate.js
+++ generate.js
@@ -4,7 +4,7 @@ module.exports = function(){
     return {
         pri: _.times(10, function(n){
             return {
-                projRef: n,
+                id: n,
                 projYear: _.random(2557, 2559),
                 projOwnerOrg: faker.company.companyName(),
                 projCode: "ID"+_.random(100000, 200000),
@@ -21,6 +21,37 @@ module.exports = function(){
                 preSaleProjCode: faker.lorem.word(),
                 projStatus: _.random(1, 3)
             }
-        })
+        }),
+        staffResource: _.times(4, function(n){
+            return {
+                id: n,
+                projResourceRef: _.random(0.0, 10.0),
+                position: faker.company.companySuffix(),
+                empNo: '00'+_.random(1000, 9999),
+                empName: faker.name.findName(),
+                dateStart: faker.date.past(),
+                dateStop: faker.date.future(),
+                duration: function(){
+                    var duration = _.range(30, 360, 30);
+                    return duration[_.random(0, duration.length-1)];
+                }(),
+                percentWork: function(){
+                    var percent = _.range(10, 100, 10);
+                    return percent[_.random(0, percent.length-1)];
+                }()
+            }
+        }),
+        program: [
+            { id: 0, programName: "ขอบเขต" },
+            { id: 1, programName: "SDE" },
+            { id: 2, programName: "เงื่อนไขโครงการ" },
+            { id: 3, programName: "งวดการส่งมอบงาน" },
+            { id: 4, programName: "ข้อจำกัด" },
+            { id: 5, programName: "Monitoring" },
+            { id: 6, programName: "ลูกค้า" },
+            { id: 7, programName: "อุปกรณ์" },
+            { id: 8, programName: "อบรม" },
+            { id: 9, programName: "อื่นๆ" }
+        ]
     }
 }
